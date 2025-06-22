d7d2b90d86f7c19afef0f7335fa5559e7357a997
Mohit Joe . R <105838181+mohitjoer@users.noreply.github.com>
2025-06-22 21:40:40 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Header from "@/components/header";
-import  Footer from "@/components/footer";
+import Footer from "@/components/footer";
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from '@clerk/themes'
-
-
-
+import { dark } from "@clerk/themes";
 
 export const metadata: Metadata = {
   title: "MarketWeb - Rent or Offer Ad Spots to Reach the Right Audience",
-  description: "MarketWeb is a platform where you can rent ad spots or offer your own space—like social communities, YouTube, or Instagram—to help businesses reach their target audience effectively.",
+  description:
+    "MarketWeb is a platform where you can rent ad spots or offer your own space—like social communities, YouTube, or Instagram—to help businesses reach their target audience effectively.",
   keywords: [
     "advertising platform",
     "rent ad spot",
@@ -21,14 +19,15 @@ export const metadata: Metadata = {
     "buy ad space",
     "digital marketing",
     "marketplace for advertising",
-    "social media ads"
+    "social media ads",
   ],
   authors: [{ name: "Mohit Joe", url: "https://github.com/mohitjoer" }],
   creator: "Mohit Joe",
   metadataBase: new URL("https://market-web-n1dz.vercel.app"),
   openGraph: {
     title: "MarketWeb - Rent or Offer Ad Spots Easily",
-    description: "Easily promote your product or service or monetize your community by renting or offering advertising spots.",
+    description:
+      "Easily promote your product or service or monetize your community by renting or offering advertising spots.",
     url: "https://market-web-n1dz.vercel.app",
     siteName: "MarketWeb",
     type: "website",
@@ -43,24 +42,21 @@ export const metadata: Metadata = {
   },
 };
 
-
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{
+    <ClerkProvider
+      appearance={{
         baseTheme: dark,
-      }}>
+      }}
+    >
       <html lang="en">
         <body>
-          <header>
-            <Header/>
-          </header>
-          <main>
-            {children}
-          </main>
+          <Header />
+          <main>{children}</main>
           <Footer />
         </body>
       </html>
